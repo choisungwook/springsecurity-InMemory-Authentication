5b5cc181f2d1c09d9088e000202db072e73b3466
choisungwook <kgg1959@naver.com>
2020-10-26 14:58:45 +0900

    Add TestA계정

--- src/main/java/SpringSecurity/Login/security/SecurityConfig.java
+++ src/main/java/SpringSecurity/Login/security/SecurityConfig.java
@@ -1,13 +1,39 @@
 package SpringSecurity.Login.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    PasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user_testA = User.builder()
+                .username("testA")
+                .password(bCryptPasswordEncoder().encode("password"))
+                .roles("TEST")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                user_testA
+        );
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
